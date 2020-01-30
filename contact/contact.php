ae00a50b31dd8762dc0c0fbff81103ead97fa44e
Leigh Daubermann <leigh@Leighs-Air.home>
2020-01-30 20:08:21 +0200

    Update

--- contact/contact.php
+++ contact/contact.php
@@ -1,17 +1,17 @@
-<?php 
-	
+<?php
+
 	$name = $_REQUEST[ 'contact-name' ];
 	$email = $_REQUEST[ 'contact-email' ];
 	$subject = $_REQUEST[ 'contact-subject' ];
 	$message = $_REQUEST[ 'contact-message' ];
 	$mail_subject = $subject . "( Customer Contact )";
 	$message = "Name: ".$name." || Email: ".$email." || Subject: ".$subject." || Message: ".$message;
-	
+
 	/* Replace YOUR_MAIL With Your Mail Address inside '' */
-	if ( mail( 'rafi.bogra@gmail.com', $mail_subject, $message, "From:" . $email ) ) {
-		echo "Thank you <strong>$name</strong> for contacting with us !!!";
+	if ( mail( 'info@qsudo.com', $mail_subject, $message, "From:" . $email ) ) {
+		echo "Thank you <strong>$name</strong> for reaching out.  We will be in touch shortly.";
 	} else {
-		echo "Error in sending message !!! Please try again";
+		echo "Error in sending message! Please try again";
 	}
-	
+
  ?>
